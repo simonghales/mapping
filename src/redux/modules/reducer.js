bd1ba1347210113ff062bc3d8599f6d0f67a9871
simonghales <simonghales@gmail.com>
2016-11-11 17:17:36 +1000

    removed immutable js

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -1,29 +1,29 @@
-// import { combineReducers } from 'redux';
-// import multireducer from 'multireducer';
-// import { routerReducer } from 'react-router-redux';
-import { combineReducers, routerReducer } from 'redux-seamless-immutable';
+import { combineReducers } from 'redux';
+import multireducer from 'multireducer';
+import { routerReducer } from 'react-router-redux';
+// import { combineReducers, routerReducer } from 'redux-seamless-immutable';
 import {reducer as reduxAsyncConnect} from 'redux-async-connect';
 
-// import auth from './auth';
-// import counter from './counter';
-// import {reducer as form} from 'redux-form';
-// import info from './info';
+import auth from './auth';
+import counter from './counter';
+import {reducer as form} from 'redux-form';
+import info from './info';
 import player from './player';
 import starred from './starred';
-// import widgets from './widgets';
+import widgets from './widgets';
 
 export default combineReducers({
   routing: routerReducer,
   reduxAsyncConnect,
-  // auth,
-  // form,
-  // multireducer: multireducer({
-  //   counter1: counter,
-  //   counter2: counter,
-  //   counter3: counter
-  // }),
-  // info,
+  auth,
+  form,
+  multireducer: multireducer({
+    counter1: counter,
+    counter2: counter,
+    counter3: counter
+  }),
+  info,
   player,
   starred,
-  // widgets,
+  widgets,
 });
